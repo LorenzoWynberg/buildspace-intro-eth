40625b72813abf91133d7bf8d8345868a9b1d67f
LorenzoWynberg <lwcincr93@gmail.com>
2022-07-09 20:53:38 -0600

    Store 👋 data on our smart contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,42 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    uint256 totalWavers;
+    address[] waversAddress;
+    mapping(address => uint256) public wavers;
+
     constructor() {
         console.log("Contract Running!");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log(wavers[msg.sender]);
+        if (wavers[msg.sender] == 0) {
+            waversAddress.push(msg.sender);
+            totalWavers += 1;
+        }
+        wavers[msg.sender] += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log(
+            "We have %s total waves by %s wavers!",
+            totalWaves,
+            totalWavers
+        );
+        return totalWaves;
+    }
+
+    function printWavers() public view {
+        for (uint256 x = 0; x < totalWavers; x++) {
+            console.log(
+                "%s Has waved %s times",
+                waversAddress[x],
+                wavers[waversAddress[x]]
+            );
+        }
     }
 }
